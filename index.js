cb84db1edcd6654110a3d9d4e07586dff8fec5a7
Pavel Naumovich <pavelnaumovic@gmail.com>
2022-11-15 18:54:33 +0000

    refactor

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ for (let i = 0; i < data.length; i++) {
 console.log("Financial Analysis");
 console.log("===================");
 console.log("Total Months: " + data.length);
-console.log("Total: " + totalProfSum);
+console.log("Total: $" + totalProfSum);
 console.log("Average  Change: " + (profDiffSum / data.length).toFixed(2));
 console.log("Greatest Increase in Profits: " + data.filter(month => month[1] == greatInc));
 console.log("Greatest Decrease in Profits: " + data.filter(month => month[1] == greatDecr));
